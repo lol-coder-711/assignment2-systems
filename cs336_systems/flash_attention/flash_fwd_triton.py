fixed fa fwd w/o causal mask

cs336_systems/flash_attention/flash_fwd_triton.py
@@ -2,6 +2,7 @@
 import torch
 import triton
 import triton.language as tl
+from jaxtyping import Float
 
 @triton.jit
 def flash_fwd_kernel(
@@ -12,11 +13,12 @@ def flash_fwd_kernel(
     stride_vb, stride_vk, stride_vd,
     stride_ob, stride_oq, stride_od,
     stride_lb, stride_lq,
-    N_QUERIES, N_KEYS,
+    N_QUERIES: tl.constexpr, N_KEYS: tl.constexpr,
     scale,
     D: tl.constexpr,
     Q_TILE_SIZE: tl.constexpr,
     K_TILE_SIZE: tl.constexpr,
+    IS_CAUSAL: tl.constexpr,
 ):
     # Program indices
     query_tile_index = tl.program_id(0)
@@ -36,7 +38,7 @@ def flash_fwd_kernel(
         K_ptr + batch_index * stride_kb,
         shape=(N_KEYS, D),
         strides=(stride_kk, stride_kd),
-        offsets=(query_tile_index * K_TILE_SIZE, 0),
+        offsets=(0, 0), # very important, as we relay on tl.advance to move K_block_ptr
         block_shape=(K_TILE_SIZE, D),
         order=(1, 0),
     )
@@ -44,7 +46,7 @@ def flash_fwd_kernel(
         V_ptr + batch_index * stride_vb,
         shape=(N_KEYS, D),
         strides=(stride_vk, stride_vd),
-        offsets=(query_tile_index * K_TILE_SIZE, 0),
+        offsets=(0, 0),
         block_shape=(K_TILE_SIZE, D),
         order=(1, 0),
     )
@@ -60,19 +62,24 @@ def flash_fwd_kernel(
         L_ptr + batch_index * stride_lb,
         shape=(N_QUERIES,),
         strides=(stride_lq,),
-        offsets=(query_tile_index * Q_TILE_SIZE, 0),
+        offsets=(query_tile_index * Q_TILE_SIZE,),
         block_shape=(Q_TILE_SIZE,),
         order=(0,),
     )
     Q_i = tl.load(Q_block_ptr)
     O_ij_prev = tl.zeros((Q_TILE_SIZE, D), dtype=tl.float32)
     l_ij_prev = tl.zeros((Q_TILE_SIZE,), dtype=tl.float32)
-    m_ij_prev = tl.full((Q_TILE_SIZE,), -tl.inf, dtype=tl.float32)
+    m_ij_prev = tl.full((Q_TILE_SIZE,), float("-inf"), dtype=tl.float32)
+    T_kv = tl.cdiv(N_KEYS, K_TILE_SIZE)
     for j in range(T_kv):
         K_j = tl.load(K_block_ptr)
         V_j = tl.load(V_block_ptr)
 
         S_ij = tl.dot(Q_i, tl.trans(K_j)) * scale
+
+        # if IS_CAUSAL:
+        #     tl.fill(S_ij, float("-inf"), tl.arange(0, Q_TILE_SIZE) >= tl.arange(0, K_TILE_SIZE))
+            
         m_ij = tl.maximum(m_ij_prev, tl.max(S_ij, axis=1))
 
         P_ij = tl.exp(S_ij - m_ij[:, None]) # expand dim of m_ij to the same shape as S_ij
@@ -84,8 +91,8 @@ def flash_fwd_kernel(
         l_ij_prev = l_ij
         m_ij_prev = m_ij
 
-        tl.advance(K_block_ptr, (K_TILE_SIZE, 0))
-        tl.advance(V_block_ptr, (K_TILE_SIZE, 0))
+        K_block_ptr = tl.advance(K_block_ptr, (K_TILE_SIZE, 0))
+        V_block_ptr = tl.advance(V_block_ptr, (K_TILE_SIZE, 0))
 
     O_i = O_ij_prev / l_ij_prev[:, None] # divide each row by l_ij_prev
     L_i = m_ij_prev + tl.log(l_ij_prev)
@@ -106,13 +113,13 @@ class FlashAttentionTriton(torch.autograd.Function):
         T_q = (Q.shape[-2] + Q_TILE_SIZE - 1) // Q_TILE_SIZE
         T_kv = (K.shape[-2] + K_TILE_SIZE - 1) // K_TILE_SIZE
         O = torch.empty_like(Q)
-        L = torch.empty(batch_size, T_q, dtype=torch.float32, device=Q.device)
+        L = torch.empty(Q.shape[:-1], dtype=torch.float32, device=Q.device)
         flash_fwd_kernel[(T_q, batch_size)](
-            Q_ptr=Q.data_ptr(),
-            K_ptr=K.data_ptr(),
-            V_ptr=V.data_ptr(),
-            O_ptr=O.data_ptr(),
-            L_ptr=L.data_ptr(),
+            Q_ptr=Q,
+            K_ptr=K,
+            V_ptr=V,
+            O_ptr=O,
+            L_ptr=L,
             stride_qb=Q.stride(0),
             stride_qq=Q.stride(1),
             stride_qd=Q.stride(2),
@@ -133,8 +140,10 @@ class FlashAttentionTriton(torch.autograd.Function):
             D=Q.shape[-1],
             Q_TILE_SIZE=Q_TILE_SIZE,
             K_TILE_SIZE=K_TILE_SIZE,
+            IS_CAUSAL=is_causal,
         )
         ctx.save_for_backward(O, L)
+        ctx.is_causal = is_causal
         return O
 
     @staticmethod