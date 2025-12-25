c8b55beb7ecc4e6162037c8b9a953119b3cdfd85
joll <joll>
2025-12-25 21:35:55 +0000

    update script

--- install_tools.sh
+++ install_tools.sh
@@ -14,3 +14,9 @@ echo "deb http://developer.download.nvidia.com/devtools/repos/ubuntu$(source /et
 apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 apt update
 apt install -y nsight-systems-cli
+
+echo "Configuring git..."
+git config --global user.name "joll"
+git config --global user.email "joll"
+
+source .venv/bin/activate
